9ab7f46d225bc303c490de84ee846803dbaafb4f
Gersi.doko <Gersi.doko@unh.edu>
2023-10-08 20:04:35 -0400

    add some extra keymappings for gitsigns
    also changed some of the highlight opts

--- lua/plugins/git_signs.lua
+++ lua/plugins/git_signs.lua
@@ -12,8 +12,12 @@ return {
       changedelete = { text = '~' },
       untracked = { text = '' },
     },
+    numhl = true,
+
     on_attach = function(bufnr)
-      vim.keymap.set('n', '<leader>hp', require('gitsigns').preview_hunk, { buffer = bufnr, desc = 'Preview git hunk' })
+      vim.keymap.set('n', '<leader>gp', require('gitsigns').preview_hunk, { buffer = bufnr, desc = 'Preview git hunk' })
+      vim.keymap.set('n', '<leader>gb', require('gitsigns').blame_line, { buffer = bufnr, desc = 'Git blame' })
+      vim.keymap.set('n', '<leader>gs', require('gitsigns').stage_buffer, { buffer = bufnr, desc = 'Stage buffer' })
 
       -- don't override the built-in and fugitive keymaps
       local gs = package.loaded.gitsigns
