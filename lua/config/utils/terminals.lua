b641fa74bc4a73670cc03c65e75465e2a6336e75
Gersi.doko <Gersi.doko@unh.edu>
2023-10-13 18:47:31 -0400

    refactor config.utils.terminals to use module based init and indexing
    Also added functionality to restart a run terminal while in the terminal buffer

--- lua/config/utils/terminals.lua
+++ lua/config/utils/terminals.lua
@@ -1,62 +1,83 @@
-local Terminal = require("toggleterm.terminal").Terminal
-local lazygit = Terminal:new({ cmd = "lazygit", direction = "float" })
-local bottom = Terminal:new({ cmd = "btop", direction = "float" })
-local gdu = Terminal:new({ cmd = "gdu" })
-local float_term = Terminal:new({ cmd = "zsh" })
-local python_term = Terminal:new({ cmd = "bpython" })
+local M = {}
+local Terminal = require('toggleterm.terminal').Terminal
 
-if jit.os == "Windows" then
-  float_term = Terminal:new({ cmd = "pwsh" })
-  python_term = Terminal:new({ cmd = "python" })
-end
-local list = {
-  lazygit = lazygit,
-  bottom = bottom,
-  gdu = gdu,
-  float = float_term,
-  python = python_term,
+---@class TerminalConfig
+M.terminals = {
+  lazygit = Terminal:new({ cmd = 'lazygit', direction = 'float' }),
+  bottom = Terminal:new({ cmd = 'btop', direction = 'float' }),
+  gdu = Terminal:new({ cmd = 'gdu', direction = 'float' }),
+  float_term = Terminal:new({ cmd = 'zsh' }),
+  python = Terminal:new({ cmd = 'bpython' }),
 }
-local close_all = function()
-  for _, term in pairs(list) do
+
+if jit.os == 'Windows' then
+  M.terminals.float_term = Terminal:new({ cmd = 'pwsh' })
+  M.terminals.python = Terminal:new({ cmd = 'python' })
+end
+
+---@type string | nil
+M.last_command = nil
+---@type Terminal | nil
+M.last_run_terminal = nil
+
+function M.lazygit()
+  M.terminals.lazygit:toggle()
+end
+
+function M.bottom()
+  M.terminals.bottom:toggle()
+end
+
+function M.gdu()
+  M.terminals.gdu:toggle()
+end
+
+function M.float()
+  M.terminals.float_term:toggle()
+end
+
+function M.python()
+  M.terminals.python:toggle()
+end
+
+function M.close_all()
+  for _, term in pairs(M.terminals) do
     term:close()
   end
 end
-return {
-  lazygit = function()
-    lazygit:toggle()
-  end,
-  bottom = function()
-    bottom:toggle()
-  end,
-  gdu = function()
-    gdu:toggle()
-  end,
-  float = function()
-    float_term:toggle()
-  end,
-  python = function()
-    python_term:toggle()
-  end,
-  close_all = close_all,
-  ---@param command string
-  run = function(command, opts)
-    if command == nil or command == "" then
-      return
-    end
-    require("toggleterm.terminal").Terminal
-      :new({
-        cmd = command,
-        hidden = true,
-        direction = opts and opts.direction or "float",
-        close_on_exit = false,
-        on_exit = function()
-          -- enter normal mode
-          vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<C-\\><C-n>", false, false, true), "n", false)
-        end,
-        float_opts = {
-          border = "curved",
-        },
-      })
-      :toggle()
-  end,
-}
+
+---@param command string | nil
+function M.run(command, opts)
+  local run_command = command or M.last_command
+  if run_command == nil or run_command == '' then
+    return
+  end
+  if M.last_run_terminal then -- close last terminal before starting a new one
+    M.last_run_terminal:close()
+  end
+  M.last_command = run_command
+  M.last_run_terminal = Terminal:new({
+    cmd = run_command,
+    hidden = true,
+    direction = opts and opts.direction or 'float',
+    close_on_exit = false,
+    on_exit = function()
+      -- enter normal mode
+      vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes('<C-\\><C-n>', false, false, true), 'n', false)
+    end,
+    on_open = function(term)
+      vim.api.nvim_buf_set_keymap(
+        term.bufnr,
+        'n',
+        '<leader>`',
+        "<cmd>lua require('config.utils.terminals').run(nil, nil)<cr>",
+        { desc = 'Run last command', noremap = true, silent = true }
+      )
+    end,
+    float_opts = {
+      border = 'curved',
+    },
+  }):toggle()
+end
+
+return M
