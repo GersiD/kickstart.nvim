c1d2ffc0f5ea58854bfc33f08f0ead0a6d245876
Gersi.doko <Gersi.doko@unh.edu>
2024-02-15 11:44:52 -0500

    format python config

--- lua/plugins/lsp/python.lua
+++ lua/plugins/lsp/python.lua
@@ -65,23 +65,24 @@ vim.api.nvim_create_autocmd({ 'BufEnter' }, {
 })
 
 return {
-  'wookayin/semshi', -- use a maintained fork
-  ft = 'python',
-  build = ':UpdateRemotePlugins',
-  enabled = false,
-  init = function()
-    -- Disabled these features better provided by LSP or other more general plugins
-    vim.g['semshi#error_sign'] = false
-    vim.g['semshi#simplify_markup'] = false
-    vim.g['semshi#mark_selected_nodes'] = false
-    vim.g['semshi#update_delay_factor'] = 0.001
-    --
-    -- This autocmd must be defined in init to take effect
-    vim.api.nvim_create_autocmd({ 'VimEnter', 'ColorScheme' }, {
-      group = vim.api.nvim_create_augroup('SemanticHighlight', {}),
-      callback = function()
-        -- Only add style, inherit or link to the LSP's colors
-        vim.cmd([[
+  {
+    'wookayin/semshi', -- use a maintained fork
+    ft = 'python',
+    build = ':UpdateRemotePlugins',
+    enabled = false,
+    init = function()
+      -- Disabled these features better provided by LSP or other more general plugins
+      vim.g['semshi#error_sign'] = false
+      vim.g['semshi#simplify_markup'] = false
+      vim.g['semshi#mark_selected_nodes'] = false
+      vim.g['semshi#update_delay_factor'] = 0.001
+      --
+      -- This autocmd must be defined in init to take effect
+      vim.api.nvim_create_autocmd({ 'VimEnter', 'ColorScheme' }, {
+        group = vim.api.nvim_create_augroup('SemanticHighlight', {}),
+        callback = function()
+          -- Only add style, inherit or link to the LSP's colors
+          vim.cmd([[
             highlight! semshiGlobal gui=italic
             highlight! link semshiImported @none
             highlight! link semshiParameter @lsp.type.parameter
@@ -92,8 +93,9 @@ return {
             highlight! link semshiUnresolved @lsp.type.unresolvedReference
             highlight! link semshiFree @none
             ]])
-      end,
-    })
-  end,
-  -- config = function() end,
+        end,
+      })
+    end,
+    -- config = function() end,
+  },
 }
